4747468e29ced82484397dd32bb1fb21bd0121ac
Jan-Philipp Willem <info@jwillem.de>
2015-08-10 20:33:46 +0200

    remove implementations

--- example/Example.js
+++ example/Example.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import {Grid, Row} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
-import NumeralInput from 'react-numeral-input';
 
 export default class Example extends Component {
   constructor(props) {
@@ -12,14 +11,11 @@ export default class Example extends Component {
     return (
       <Grid>
         <Row>
-          <h1>Numeral-Input-Examples</h1>
+          <h1>Component-Examples</h1>
         </Row>
         <Row>
-          NumeralInput-Component with success and large and defaultValue
-          <NumeralInput bsStyle='success' bsSize='large' value='10'/>
-          NumeralInput-Component with defaultValue and numeralFormat
-          <NumeralInput value='10.2' numeralFormat='0,0.0'/>
-       </Row>
+          // put examples here
+        </Row>
       </Grid>
     );
   }
